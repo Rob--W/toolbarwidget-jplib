d9dd1c59746cda323053711a1d66b79a9fae8e2f
Rob Wu <gwnRob@gmail.com>
2013-11-08 15:27:27 +0100

    Add tests for aspectRatio and autoShrink

--- tests/test-toolbarwidget.js
+++ tests/test-toolbarwidget.js
@@ -42,6 +42,47 @@ exports['test widget on nav-bar'] = function(assert, done) {
         }
     }, ASYNC_TEST_TIMEOUT);
 };
+exports['test widget on nav-bar, autoShrink and aspectRatio'] = function(assert, done) {
+    let finished = false;
+
+    let toolbarID = 'nav-bar';
+    let widgetID = 'shrunk-and-aspect-ratio';
+    let UNREASONABLE_HIGH_HEIGHT = 400;
+    let toolbarWidget;
+    let aspectRatio = 2;
+    var options = {
+        toolbarID: toolbarID,
+        id: widgetID,
+        label: 'My Mozilla Widget',
+        contentURL: 'http://www.mozilla.org/favicon.ico',
+
+        height: UNREASONABLE_HIGH_HEIGHT,
+        //autoShrink: true, = default
+        aspectRatio: aspectRatio,
+        width: 300, // Should be ignored because of aspect ratio
+        onAttach: function() {
+            let toolbar = $(toolbarID);
+            assert.ok(!!toolbar, 'toolbar exists for toolbarID "' + toolbarID + '"');
+
+            let widget = $(toolbarwidget.getWidgetId(widgetID));
+            assert.ok(!!widget, 'widget element found');
+            assert.ok(toolbar.contains(widget), 'Toolbar contains widget');
+            assert.ok(widget.clientHeight < UNREASONABLE_HIGH_HEIGHT, 'Widget height should be reasonably small when autoShrink is true');
+            assert.ok(Math.abs(aspectRatio - widget.clientHeight / widget.clientWidth) < 0.1, 'Aspect ratio should be respected');
+            finished = true;
+            toolbarWidget.destroy();
+            done();
+        }
+    };
+    toolbarWidget = toolbarwidget.ToolbarWidget(options);
+
+    // Stop the test when it takes too long...
+    require('sdk/timers').setTimeout(function() {
+        if (!finished) {
+            throw new Error('Test takes too long. onAttach was not called within ' + ASYNC_TEST_TIMEOUT + 'ms.');
+        }
+    }, ASYNC_TEST_TIMEOUT);
+};
 exports['test widget on toolbar'] = function(assert, done) {
     let finished = false;
 
@@ -52,6 +93,7 @@ exports['test widget on toolbar'] = function(assert, done) {
         label: 'My Mozilla Widget',
         contentURL: 'http://www.mozilla.org/favicon.ico',
         height: 10,
+        autoShrink: false,
         onAttach: function() {
             let widget = $(toolbarwidget.getWidgetId(widgetID));
             assert.ok(!!widget, 'widget element found');
